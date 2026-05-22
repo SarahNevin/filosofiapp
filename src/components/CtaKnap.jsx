f5450cde78a1e606387093826a72537ef59e4bac
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-23 01:02:44 +0200

    styles tilføjet

--- src/components/CtaKnap.jsx
+++ src/components/CtaKnap.jsx
@@ -1,7 +1,7 @@
 export function CtaKnap({ children, onClick }) {
     return (
         <button
-            className="text-xl md:text-[26px] font-medium text-[#722E3C] border-2 rounded-[30px] overflow-hidden transition-colors duration-300 hover:text-white py-1 px-6 flex justify-center bg-[#CEBC9833]"
+            className="w-[300px] rounded-[12px] bg-[#F3E9DC] text-[#151B22] py-3"
             onClick={onClick}
         >
             {children}
