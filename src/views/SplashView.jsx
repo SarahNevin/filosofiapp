170f27f853a1755740fac13a88a233a02f22632d
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-23 18:04:38 +0200

    CTA-knap slettet

--- src/views/SplashView.jsx
+++ src/views/SplashView.jsx
@@ -1,19 +1,4 @@
-import { CtaKnap } from "../components/CtaKnap";
 import GyldendalLogo from "../assets/images/gyldendal-logo.svg";
 
 export default function SplashView({ onStart }) {
-    return (
-        <div>
-            <div className="flex flex-col items-center gap-6">
-                <CtaKnap onClick={onStart}>
-                    START REJSEN
-                </CtaKnap>
-            </div>
-                <img
-                src={GyldendalLogo}
-                alt="Gyldendal logo"
-                className="absolute bottom-4 left-4 w-24"
-            />
-        </div>
-    );
 }
