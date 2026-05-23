ebbbf80e96814cb6cef0f14bdfc7c3abcd0c9e93
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-23 19:38:31 +0200

    Styles for logo ændret + timer sat på så der automatisk skiftes til næste screen

--- src/views/SplashView.jsx
+++ src/views/SplashView.jsx
@@ -1,4 +1,26 @@
-import GyldendalLogo from "../assets/images/gyldendal-logo.svg";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import GyldendalLogo from "../assets/images/gyldendal-logo.svg";  
 
-export default function SplashView({ onStart }) {
+export default function SplashView() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/onboarding");
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
+  return (
+    <div classname="flex items-center justify-center min-h-screen">
+        <h1>Sofies Verden</h1>
+        <img
+            src={GyldendalLogo}
+            alt="Gyldendal logo"
+            className="absolute bottom-4 left-1/2 -translate-x-1/2 w-24"
+        />
+    </div>
+  );
 }
