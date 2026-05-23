23c8960b3517056ae2734b190feacc3c23073d23
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-23 22:05:30 +0200

    Tekst tilføjet

--- src/views/OnboardingView.jsx
+++ src/views/OnboardingView.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { CtaKnap } from "../components/CtaKnap";
 import GyldendalLogo from "../assets/images/gyldendal-logo.svg";  
 import ScreenHeader from "../components/ScreenHeader";
+import Tekstblok from "../components/Tekstblok";
 
 export default function OnboardingView() {
     const navigate = useNavigate();
@@ -12,6 +13,11 @@ export default function OnboardingView() {
             tagline="Opdag verden med nye øjne"
             title="Sofies Verden"
             level="h1"
+            />
+            <Tekstblok
+            title="Velkommen, tænker ..."
+            text="Dyk ned i fortællinger, idéer og filosofiske spørgsmål fra Sofies Verden ét tema ad gangen."
+            level="h2"
             />
             <div className="flex flex-col items-center gap-6">
                 <CtaKnap onClick={() => navigate("/intro")}>
