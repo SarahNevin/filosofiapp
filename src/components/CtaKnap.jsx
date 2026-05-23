27cfd0b66c58995b1c99a12c971c223ffb44e879
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-23 19:56:57 +0200

    Styles på CTA-knap tilføjet

--- src/components/CtaKnap.jsx
+++ src/components/CtaKnap.jsx
@@ -1,7 +1,7 @@
 export function CtaKnap({ children, onClick }) {
     return (
         <button
-            className="w-[300px] rounded-[12px] bg-[#F3E9DC] text-[#151B22] py-3"
+            className="w-[300px] min-h-[81px] rounded-[12px] bg-[#F3E9DC] text-[#151B22] py-5 flex items-center justify-center text-center CTA-tekst"
             onClick={onClick}
         >
             {children}
