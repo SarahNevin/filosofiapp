6fbf871b643183687fa9f4de3d00222bb580379c
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-23 21:43:55 +0200

    Screenheader-javascript tilføjet

---
+++ src/components/ScreenHeader.jsx
@@ -0,0 +1,17 @@
+export default function ScreenHeader({
+    tagline,
+    title,
+    level ="h1"
+}) {
+    const isHeading1 = level === "h1";
+
+    return (
+        <header className={`pt-16 flex flex-col items-center ${isHeading1 ? "gap-7" : "gap-3"}`}>
+        <h3>{tagline}</h3>
+
+        {isHeading1 ? (
+            <h1>{title}</h1>) : (<h2>{title}</h2>
+        )}
+        </header>
+    );
+    }
