d3eef4093456bb8014d728f912425e675c5e3c94
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-25 01:32:27 +0200

    Formatering

--- src/components/LessonCard.jsx
+++ src/components/LessonCard.jsx
@@ -1,19 +1,16 @@
-function LessonCard ({ title, image }) {
+function LessonCard({ title, image }) {
   return (
-
     <div className="w-full h-full overflow-hidden relative rounded-b-[12px]">
+      <img src={image} alt={title} className="w-full h-[225px] object-cover" />
 
-        <img src={image} alt={title} className="w-full h-[225px] object-cover" />
-
-        <div className="absolute bottom-0 left-0 right-0 h-[75px] flex items-center justify-center rounded-b-[12px]"
-          style={{ backgroundColor: "#F3E9DC" }}
+      <div
+        className="absolute bottom-0 left-0 right-0 h-[75px] flex items-center justify-center rounded-b-[12px]"
+        style={{ backgroundColor: "#F3E9DC" }}
       >
-        <p className="emne-tekst">
-          {title}
-        </p>
-        </div>
+        <p className="emne-tekst">{title}</p>
+      </div>
     </div>
   );
 }
 
-export default LessonCard;    
+export default LessonCard;
