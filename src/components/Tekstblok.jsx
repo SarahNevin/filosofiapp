b520e5cfc4d930c6f3935a7617f2c13f45c04f13
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-24 03:08:28 +0200

    text-string erstattet med children for at gøre indholdet mere fleksibelt

--- src/components/Tekstblok.jsx
+++ src/components/Tekstblok.jsx
@@ -1,10 +1,10 @@
-export default function Tekstblok({ title, text, level = "h1" }) {
+export default function Tekstblok({ title, text, level = "h1", children }) {
   const isH1 = level === "h1";
 
   return (
-    <section className="my-[30px] flex flex-col items-center gap-6 px-14">
+    <section className="my-[30px] flex flex-col items-center gap-6 px-14 text-center">
       {isH1 ? <h1>{title}</h1> : <h2>{title}</h2>}
-      <p>{text}</p>
+      <div className="flex flex-col gap-4">{children}</div>
     </section>
   );
 }
