039c4ccf5ef8b99323f5096c777ff6940ebf3c60
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-24 02:34:39 +0200

    ScreenHeader + billede tilføjet

--- src/views/EmneView.jsx
+++ src/views/EmneView.jsx
@@ -1,6 +1,23 @@
+import ScreenHeader from "../components/ScreenHeader";
+import platonHule from "../assets/images/platons-hule.webp";
+
 export default function EmneView() {
   return (
     <div>
+      <ScreenHeader
+        tagline="Platon"
+        title="Ud af hulen"
+        level="h1"
+        visTilbageknap={true}
+      />
+
+      <div className="w-full flex justify-center mt-6">
+        <img
+          src={platonHule}
+          alt="Platons hule"
+          className="w-full h-[300px] object-cover"
+        />
+      </div>
     </div>
   );
 }
--- src/views/HistorieView.jsx
+++ src/views/HistorieView.jsx
@@ -1,6 +1,23 @@
+import ScreenHeader from "../components/ScreenHeader";
+import platonHule from "../assets/images/platons-hule.webp";
+
 export default function HistorieView() {
   return (
     <div>
+      <ScreenHeader
+        tagline="Oplev fortællingen"
+        title="Ud af hulen"
+        level="h2"
+        visTilbageknap={true}
+      />
+
+      <div className="w-full flex justify-center mt-6">
+        <img
+          src={platonHule}
+          alt="Platons hule"
+          className="w-full h-[300px] object-cover"
+        />
+      </div>
     </div>
   );
 }
