c79f8fb23941fdba16890dfcb90ce1d6e5da98a3
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-24 03:58:53 +0200

    Knapper tilføjet

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,30 @@
 import { useNavigate } from "react-router-dom";
 
 export default function Navigation() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-         <nav className="fixed bottom-0 left-0 w-full h-[91px] bg-[#151B22] flex justify-around items-center">
+  return (
+    <nav className="fixed bottom-0 left-0 w-full h-[91px] bg-[#151B22] flex justify-around items-center">
+      <button
+        onClick={() => navigate("/onboarding")}
+        className="flex flex-col items-center justify-center gap-1 text-[#B4B0B0]"
+      >
+        <span className="navbar-tekst">Hjem</span>
+      </button>
 
+      <button
+        onClick={() => navigate("/onboarding")}
+        className="flex flex-col items-center justify-center gap-1 text-[#B4B0B0]"
+      >
+        <span className="navbar-tekst">Søg</span>
+      </button>
 
-         </nav>
-    );
+      <button
+        onClick={() => navigate("/profile")}
+        className="flex flex-col items-center justify-center gap-1 text-[#B4B0B0]"
+      >
+        <span className="navbar-tekst">Profil</span>
+      </button>
+    </nav>
+  );
 }
