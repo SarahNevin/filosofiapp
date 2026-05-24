5fc541a9392a78c583d9d4d9959a849b7e6e8195
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-25 01:16:16 +0200

    html + styling til cards i slider

---
+++ src/components/LessonCard.jsx
@@ -0,0 +1,19 @@
+function LessonCard ({ title, image }) {
+  return (
+
+    <div className="w-full h-full overflow-hidden relative rounded-b-[12px]">
+
+        <img src={image} alt={title} className="w-full h-[225px] object-cover" />
+
+        <div className="absolute bottom-0 left-0 right-0 h-[75px] flex items-center justify-center rounded-b-[12px]"
+          style={{ backgroundColor: "#F3E9DC" }}
+      >
+        <p className="emne-tekst">
+          {title}
+        </p>
+        </div>
+    </div>
+  );
+}
+
+export default LessonCard;    
