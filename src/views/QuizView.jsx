5a58d5fdd112c3e2008e27d530c614a0aa579489
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-24 21:22:43 +0200

    Indhold tilføjet til popupboks: Knapper + hinttekst

--- src/views/QuizView.jsx
+++ src/views/QuizView.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import { CtaKnap } from "../components/CtaKnap";
 
 import Navigation from "../components/Navigation";
 import Guidetekst from "../components/Guidetekst";
+import LydIkon from "../assets/images/lyd.svg";
 
 export default function QuizView() {
   const [popupVisible, setPopupVisible] = useState(false);
@@ -24,16 +26,39 @@ export default function QuizView() {
           onClick={() => setPopupVisible(false)}
         >
           <div
-            className="relative w-full max-w-[320px] rounded-[20px] bg-black px-4 py-14"
+            className="relative w-full max-w-[320px] rounded-[20px] px-4 py-14 pb-10"
             onClick={(e) => e.stopPropagation()}
           >
-        {/* Luk-knap */}
-        <button onClick={() => setPopupVisible(false)}
-          className="absolute top-4 right-4 text-5xl">
-            x
-        </button>
+            {/* Luk-knap */}
+            <button
+              onClick={() => setPopupVisible(false)}
+              className="absolute top-4 right-4 text-5xl text-[#B4B0B0]"
+            >
+              x
+            </button>
+
+            {/* Hint-knapper i layover */}
+            <div className="pt-6">
+            <div className="flex flex-col gap-5">
+              <CtaKnap>
+                <span className="flex items-center gap-3">
+                  <img src={LydIkon} alt="" className="w-7 h-7" />
+                  <span>Hvad tænker Ida, 11 år?</span>
+                </span>
+              </CtaKnap>
+
+              <CtaKnap>Få historien fortalt igen</CtaKnap>
+            </div>
+
+            {/* Hint */}
+            <p
+              className="mt-6 text-center"
+            >
+              Hint: Find svaret i Sofies Verden s. 122, andet afsnit
+            </p>
+          </div>
+          </div>
         </div>
-      </div>
       )}
 
       <Navigation />
