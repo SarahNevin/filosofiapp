98e9625cda18dc681653ec561aafaad6002ea29d
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-24 02:35:10 +0200

    Delkomponenten tilbagepil tilføjet

--- src/components/ScreenHeader.jsx
+++ src/components/ScreenHeader.jsx
@@ -1,17 +1,26 @@
+import Tilbageknap from "./Tilbageknap";
+
 export default function ScreenHeader({
-    tagline,
-    title,
-    level ="h1"
+  tagline,
+  title,
+  level = "h1",
+  visTilbageknap = false,
 }) {
-    const isHeading1 = level === "h1";
+  const isHeading1 = level === "h1";
 
-    return (
-        <header className={`pt-16 flex flex-col items-center mb-20 ${isHeading1 ? "gap-7" : "gap-3"}`}>
-        <h3>{tagline}</h3>
+  return (
+    <header
+      className={`pt-16 flex flex-col items-center mb-20 ${isHeading1 ? "gap-7" : "gap-3"}`}
+    >
+      <h3>{tagline}</h3>
 
-        {isHeading1 ? (
-            <h1>{title}</h1>) : (<h2>{title}</h2>
-        )}
-        </header>
-    );
-    }
+      <div className="relative w-full">
+        {visTilbageknap && <Tilbageknap />}
+
+        <div className="flex justify-center">
+          {isHeading1 ? <h1>{title}</h1> : <h2>{title}</h2>}
+        </div>
+      </div>
+    </header>
+  );
+}
