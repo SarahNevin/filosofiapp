313a29d8e831776f1cc1b676c7e8bd85c57f9d68
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-24 03:35:12 +0200

    Tailwind-styles til navbar

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
+export default function Navigation() {
+    const navigate = useNavigate();
+
+    return (
+         <nav className="fixed bottom-0 left-0 w-full h-[91px] bg-[#151B22] flex justify-around items-center">
+
+
+         </nav>
+    );
+}
