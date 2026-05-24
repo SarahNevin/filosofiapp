78cf146b1bc9613dfec778b8d74fedd001d47bab
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-24 20:10:59 +0200

    Guidetekst med animeret trane tilføjet

--- src/views/QuizView.jsx
+++ src/views/QuizView.jsx
@@ -1,8 +1,16 @@
 import Navigation from "../components/Navigation";
+import Guidetekst from "../components/Guidetekst";
 
 export default function QuizView() {
   return (
     <div className="min-h-screen pb-[91px]">
+      <Guidetekst
+        kanKlikkes={true}
+        animated={true}
+      >
+        “Tryk på mig, hvis du vil have et hint.”
+      </Guidetekst>
+
       <Navigation />
     </div>
   );
