ad6b25336b57ae6beac8c0780eea0cd28fab5e2d
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-24 18:45:27 +0200

    kanKlikkes-prop tilføjet, så tranen kan være både klikbar og statisk

--- src/components/Guidetekst.jsx
+++ src/components/Guidetekst.jsx
@@ -4,16 +4,26 @@ export default function Guidetekst({
   children,
   animated = false,
   onBirdClick,
+  kanKlikkes = false,
 }) {
+
+    const traneIndhold = (
+    <div className={`shrink-0 ${animated ? "animate-bird" : ""}`}>
+      <img src={Trane} alt="Tranen Ori" className="w-[150px]" />
+    </div>
+  );
+
   return (
     <div className="mt-8 flex items-center justify-between pl-5">
       <p className="flex-1">{children}</p>
-      <button
-        onClick={onBirdClick}
-        className={`shrink-0 ${animated ? "animate-bird" : ""}`}
-      >
-        <img src={Trane} alt="Tranen Ori" className="w-[150px]" />
-      </button>
+
+    {kanKlikkes && onBirdClick ? (
+        <button onClick={onBirdClick} type="button">
+          {traneIndhold}
+        </button>
+      ) : (
+        traneIndhold
+      )}
     </div>
   );
 }
