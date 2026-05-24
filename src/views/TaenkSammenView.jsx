622d58e14c4e8d3003e62d5ebc1d1fb2965b1f99
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-24 03:21:48 +0200

    Tekst til screenHeader og tekstblok tilføjer

--- src/views/TaenkSammenView.jsx
+++ src/views/TaenkSammenView.jsx
@@ -1,6 +1,34 @@
+import ScreenHeader from "../components/ScreenHeader";
+import Tekstblok from "../components/Tekstblok";
+
 export default function TaenkSammenView() {
   return (
     <div>
+      <ScreenHeader
+        tagline="Tænk sammen"
+        title="Platons hule"
+        level="h2"
+        visTilbageknap
+      />
+
+      {/* Tekstblok 1 */}
+      <Tekstblok title="Rollespil" level="h1">
+        <p>
+          Vælg en rolle og spil hulelignelsen sammen i klassen. Hvordan oplever
+          I sandheden forskelligt?
+        </p>
+      </Tekstblok>
+
+      {/* Tekstblok 2 */}
+      <Tekstblok title="Overvej dette" level="h2">
+        <ul className="space-y-3 list-disc">
+          <li>Hvem afgør, hvad der er sandt?</li>
+          <li>Hvorfor kan nye idéer være svære at acceptere?</li>
+          <li>Kan man stole på sine sanser?</li>
+          <li>Ville du turde forlade hulen?</li>
+          <li>Hvordan er det, at være alene om at se noget anderledes?</li>
+        </ul>
+      </Tekstblok>
     </div>
   );
 }
