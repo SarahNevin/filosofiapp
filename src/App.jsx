a67ff33c430e4039070439f3bf94e5f4f0a9b5d0
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-24 02:38:34 +0200

    Filer omdøbt

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import EmneView from "./views/EmneView";
 import HistorieView from "./views/HistorieView";
-import IntroSliderView from "./views/IntroSliderView";
+import VaelgEmneView from "./views/VaelgEmneSliderView";
 import OnboardingView from "./views/OnboardingView";
 import QuizView from "./views/QuizView";
 import SplashView from "./views/SplashView";
