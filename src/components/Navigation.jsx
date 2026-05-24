33ee26f56ff8cf8537e36acfa3e7192a540c6eb8
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-24 16:15:04 +0200

    Farve på ikoner slettet (overflødigt)

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -11,7 +11,7 @@ export default function Navigation() {
     <nav className="fixed bottom-0 left-0 w-full h-[91px] bg-[#151B22] flex justify-around items-center">
       <button
         onClick={() => navigate("/onboarding")}
-        className="flex flex-col items-center justify-center gap-1 text-[#B4B0B0]"
+        className="flex flex-col items-center justify-center gap-1"
       >
         <img src={HjemIkon} alt="Hjem" className="w-[25px] h-[25px]" />
         <span className="navbar-tekst">Hjem</span>
@@ -19,7 +19,7 @@ export default function Navigation() {
 
       <button
         onClick={() => navigate("/onboarding")}
-        className="flex flex-col items-center justify-center gap-1 text-[#B4B0B0]"
+        className="flex flex-col items-center justify-center gap-1"
       >
         <img src={SoegIkon} alt="Søg" className="w-[25px] h-[25px]" />
         <span className="navbar-tekst">Søg</span>
@@ -27,7 +27,7 @@ export default function Navigation() {
 
       <button
         onClick={() => navigate("/profile")}
-        className="flex flex-col items-center justify-center gap-1 text-[#B4B0B0]"
+        className="flex flex-col items-center justify-center gap-1"
       >
         <img src={ProfilIkon} alt="Profil" className="w-[25px] h-[25px]" />
         <span className="navbar-tekst">Profil</span>
