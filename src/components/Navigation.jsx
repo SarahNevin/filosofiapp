671f6515bdf78794270305b2ea4b168c42eefc43
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-25 19:46:39 +0200

    Ikke-fungerende links rettet

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,8 @@
 import { useNavigate } from "react-router-dom";
 
-import HjemIkon from "../assets/images/hjem.svg";
-import SoegIkon from "../assets/images/soeg.svg";
-import ProfilIkon from "../assets/images/profil.svg";
+import hjemIkon from "../assets/images/hjem.svg";
+import soegIkon from "../assets/images/soeg.svg";
+import profilIkon from "../assets/images/profil.svg";
 
 export default function Navigation() {
   const navigate = useNavigate();
@@ -13,23 +13,23 @@ export default function Navigation() {
         onClick={() => navigate("/onboarding")}
         className="flex flex-col items-center justify-center gap-1"
       >
-        <img src={HjemIkon} alt="Hjem" className="w-[25px] h-[25px]" />
+        <img src={hjemIkon} alt="Hjem" className="w-[25px] h-[25px]" />
         <span className="navbar-tekst">Hjem</span>
       </button>
 
       <button
-        onClick={() => navigate("/onboarding")}
+        onClick={() => {}}
         className="flex flex-col items-center justify-center gap-1"
       >
-        <img src={SoegIkon} alt="Søg" className="w-[25px] h-[25px]" />
+        <img src={soegIkon} alt="Søg" className="w-[25px] h-[25px]" />
         <span className="navbar-tekst">Søg</span>
       </button>
 
       <button
-        onClick={() => navigate("/profile")}
+        onClick={() => {}}
         className="flex flex-col items-center justify-center gap-1"
       >
-        <img src={ProfilIkon} alt="Profil" className="w-[25px] h-[25px]" />
+        <img src={profilIkon} alt="Profil" className="w-[25px] h-[25px]" />
         <span className="navbar-tekst">Profil</span>
       </button>
     </nav>
