3e08c64fc853a0c8d21c29135aa1a04e758c0c4d
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-25 03:10:23 +0200

    Link tilføjet og card gjort klikbart

--- src/components/LessonCard.jsx
+++ src/components/LessonCard.jsx
@@ -1,15 +1,19 @@
-function LessonCard({ title, image }) {
+import { Link } from "react-router-dom";
+
+function LessonCard({ title, image, link }) {
   return (
+    <Link to={link} className="block w-[332px] h-[300px]">
     <div className="w-full h-full overflow-hidden relative rounded-b-[12px]">
       <img src={image} alt={title} className="w-full h-[225px] object-cover" />
 
       <div
-        className="absolute bottom-0 left-0 right-0 h-[75px] flex items-center justify-center rounded-b-[12px]"
+        className="absolute bottom-0 left-0 right-0 h-[75px] flex items-center justify-center"
         style={{ backgroundColor: "#F3E9DC" }}
       >
         <p className="emne-tekst">{title}</p>
       </div>
     </div>
+  </Link>
   );
 }
 
