0b1facf8d58ba72dd795530c97b1b5e1ac747434
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-26 20:36:25 +0200

    Quizdata med spørgsmål tilføjet

---
+++ src/data/quizData.js
@@ -0,0 +1,94 @@
+const quizData = [
+  {
+    question: "Hvad symboliserer hulen?",
+    answers: [
+      {
+        text: "Begrænset viden",
+        correct: true,
+      },
+      {
+        text: "Et hemmeligt skjulested",
+        correct: false,
+      },
+      {
+        text: "Et underjordisk fængsel",
+        correct: false,
+      },
+    ],
+  },
+
+  {
+    question: "Hvad symboliserer skyggerne?",
+    answers: [
+      {
+        text: "Monstre",
+        correct: false,
+      },
+      {
+        text: "Illusioner",
+        correct: true,
+      },
+      {
+        text: "Drømme",
+        correct: false,
+      },
+    ],
+  },
+
+  {
+    question: "Hvad opdager den frigjorte?",
+    answers: [
+      {
+        text: "En skat",
+        correct: false,
+      },
+      {
+        text: "Sandheden",
+        correct: true,
+      },
+      {
+        text: "Et nyt fængsel",
+        correct: false,
+      },
+    ],
+  },
+
+  {
+    question: "Hvad repræsenterer solen uden for hulen?",
+    answers: [
+      {
+        text: "Viden og indsigt",
+        correct: true,
+      },
+      {
+        text: "En farlig ild",
+        correct: false,
+      },
+      {
+        text: "Et andet fængsel",
+        correct: false,
+      },
+    ],
+  },
+
+  {
+    question:
+      "Hvordan reagerer fangerne, når nogen fortæller om verden udenfor?",
+    answers: [
+      {
+        text: "De bliver nysgerrige og følger med",
+        correct: false,
+      },
+      {
+        text: "De forlader straks hulen",
+        correct: false,
+      },
+      {
+        text: "De tror det er løgn og afviser det",
+        correct: true,
+      },
+    ],
+  },
+];
+
+export default quizData;
