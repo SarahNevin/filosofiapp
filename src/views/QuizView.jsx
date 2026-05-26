540aa58b2454bed160a5c1f92d7e97cd24d20983
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-26 22:14:31 +0200

    quizdata importeret & usestate + styles til spørgsmål tilføjet

--- src/views/QuizView.jsx
+++ src/views/QuizView.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { CtaKnap } from "../components/CtaKnap";
+import quizData from "../data/quizData";
 
 import Navigation from "../components/Navigation";
 import ScreenHeader from "../components/ScreenHeader";
@@ -8,6 +9,9 @@ import Guidetekst from "../components/Guidetekst";
 import LydIkon from "../assets/images/lyd.svg";
 
 export default function QuizView() {
+  const [currentQuestion] = useState(0);
+  const question = quizData[currentQuestion];
+
   const [popupVisible, setPopupVisible] = useState(false);
   const navigate = useNavigate();
 
@@ -35,58 +39,74 @@ export default function QuizView() {
         </div>
       </div>
 
-        {/* Guidetekst med klikbar trane for hint */}
-        <Guidetekst
-          kanKlikkes={true}
-          animated={true}
-          onBirdClick={() => setPopupVisible(true)}
-        >
-          “Tryk på mig, hvis du vil have et hint.”
-        </Guidetekst>
+      {/* SPØRGSMÅL */}
+      <h1 className="spoergsmaal-tekst text-center mb-3 px-5">
+        {question.question}
+      </h1>
 
-        {/* Popup ved klik på trane */}
-        {popupVisible && (
-          <div
-            className=" fixed inset-0 z-50 flex items-center justify-center bg-black/40 px-6"
-            onClick={() => setPopupVisible(false)}
+      <div className="flex flex-col gap-6 mt-6 px-10">
+        {question.answers.map((answer, index) => (
+          <button
+            key={index}
+            className="border border-[#F3E9DC] p-4 rounded-xl shadow"
           >
-            <div
-              className="relative w-full max-w-[320px] rounded-[20px] px-4 py-14 pb-10"
-              onClick={(e) => e.stopPropagation()}
+            {answer.text}
+          </button>
+        ))}
+      </div>
+
+      {/* Guidetekst med klikbar trane for hint */}
+      <Guidetekst
+        kanKlikkes={true}
+        animated={true}
+        onBirdClick={() => setPopupVisible(true)}
+      >
+        “Tryk på mig, hvis du vil have et hint.”
+      </Guidetekst>
+
+      {/* Popup ved klik på trane */}
+      {popupVisible && (
+        <div
+          className=" fixed inset-0 z-50 flex items-center justify-center bg-black/40 px-6"
+          onClick={() => setPopupVisible(false)}
+        >
+          <div
+            className="relative w-full max-w-[320px] rounded-[20px] px-4 py-14 pb-10"
+            onClick={(e) => e.stopPropagation()}
+          >
+            {/* Luk-knap */}
+            <button
+              onClick={() => setPopupVisible(false)}
+              className="absolute top-4 right-4 text-5xl text-[#B4B0B0]"
             >
-              {/* Luk-knap */}
-              <button
-                onClick={() => setPopupVisible(false)}
-                className="absolute top-4 right-4 text-5xl text-[#B4B0B0]"
-              >
-                x
-              </button>
+              x
+            </button>
 
-              {/* Hint-knapper i layover */}
-              <div className="pt-6">
-                <div className="flex flex-col gap-5">
-                  <CtaKnap>
-                    <span className="flex items-center gap-3">
-                      <img src={LydIkon} alt="" className="w-7 h-7" />
-                      <span>Hvad tænker Ida, 11 år?</span>
-                    </span>
-                  </CtaKnap>
+            {/* Hint-knapper i layover */}
+            <div className="pt-6">
+              <div className="flex flex-col gap-5">
+                <CtaKnap>
+                  <span className="flex items-center gap-3">
+                    <img src={LydIkon} alt="" className="w-7 h-7" />
+                    <span>Hvad tænker Ida, 11 år?</span>
+                  </span>
+                </CtaKnap>
 
-                  <CtaKnap onClick={() => navigate("/historie")}>
-                    Få historien fortalt igen
-                  </CtaKnap>
-                </div>
-
-                {/* Hint */}
-                <p className="mt-6 px-6 text-center">
-                  Hint: Find svaret i Sofies Verden s. 122, andet afsnit
-                </p>
+                <CtaKnap onClick={() => navigate("/historie")}>
+                  Få historien fortalt igen
+                </CtaKnap>
               </div>
+
+              {/* Hint */}
+              <p className="mt-6 px-6 text-center">
+                Hint: Find svaret i Sofies Verden s. 122, andet afsnit
+              </p>
             </div>
           </div>
-        )}
+        </div>
+      )}
 
-        <Navigation />
-      </div>
+      <Navigation />
+    </div>
   );
 }
