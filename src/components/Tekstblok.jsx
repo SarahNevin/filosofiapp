5160bf7aafff5f64f71bb2c60d969deaa0625f6d
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-27 14:24:18 +0200

    Mulighed for at fleksibel margin (y) tilføjet

--- src/components/Tekstblok.jsx
+++ src/components/Tekstblok.jsx
@@ -1,8 +1,8 @@
-export default function Tekstblok({ title, level = "h1", children }) {
+export default function Tekstblok({ title, level = "h1", children, className }) {
   const isH1 = level === "h1";
 
   return (
-    <section className="my-[30px] flex flex-col items-center gap-6 px-14 text-center">
+    <section className={`flex flex-col items-center gap-6 px-14 text-center ${className}`}>
       {isH1 ? <h1>{title}</h1> : <h2>{title}</h2>}
       <div className="flex flex-col gap-4 text-left">{children}</div>
     </section>
