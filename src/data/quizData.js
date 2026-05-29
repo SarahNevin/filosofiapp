30982afdc69c8873e0252720746c33b8d79ec5d3
SarahNevin <eaa25sjn@students.eaaa.dk>
2026-05-29 15:59:48 +0200

    Hint-tekst fra pop-up flyttet over i data, så teksten kan varieres fra spørgsmål til spørgsmål

--- src/data/quizData.js
+++ src/data/quizData.js
@@ -1,93 +1,69 @@
 const quizData = [
   {
     question: "Hvad symboliserer hulen?",
-    answers: [
-      {
-        text: "Begrænset viden",
-        correct: true,
-      },
-      {
-        text: "Et hemmeligt skjulested",
-        correct: false,
-      },
-      {
-        text: "Et underjordisk fængsel",
-        correct: false,
-      },
+   answers: [
+      { text: "Begrænset viden", correct: true },
+      { text: "Et hemmeligt skjulested", correct: false },
+      { text: "Et underjordisk fængsel", correct: false },
     ],
+    hints: {
+      audioText: "Hvad tænker Ida, 11 år?",
+      hint: "Hint: Find svaret i Sofies Verden s. 95, tredje afsnit",
+    },
+
   },
 
   {
     question: "Hvad symboliserer skyggerne?",
     answers: [
-      {
-        text: "Monstre",
-        correct: false,
-      },
-      {
-        text: "Illusioner",
-        correct: true,
-      },
-      {
-        text: "Drømme",
-        correct: false,
-      },
+      { text: "Monstre", correct: false },
+      { text: "Illusioner", correct: true },
+      { text: "Drømme", correct: false },
     ],
+    hints: {
+      audioText: "Hvad tænker Emil, 10 år?",
+      hint: "Hint: Find svaret i Sofies Verden s. 95, tredje afsnit",
+    },
   },
+
 
   {
     question: "Hvad opdager den frigjorte?",
     answers: [
-      {
-        text: "En skat",
-        correct: false,
-      },
-      {
-        text: "Sandheden",
-        correct: true,
-      },
-      {
-        text: "Et nyt fængsel",
-        correct: false,
-      },
+      { text: "En skat", correct: false },
+      { text: "Sandheden", correct: true },
+      { text: "Et nyt fængsel", correct: false },
     ],
+    hints: {
+      audioText: "Hvad tænker Nora, 11 år?",
+      hint: "Hint: Find svaret i Sofies Verden s. 95, fjerde afsnit",
+    },
   },
 
   {
     question: "Hvad repræsenterer solen uden for?",
     answers: [
-      {
-        text: "Viden og indsigt",
-        correct: true,
-      },
-      {
-        text: "En farlig ild",
-        correct: false,
-      },
-      {
-        text: "Et andet fængsel",
-        correct: false,
-      },
+      { text: "Viden og indsigt", correct: true },
+      { text: "En farlig ild", correct: false },
+      { text: "Et andet fængsel", correct: false },
     ],
+    hints: {
+      audioText: "Hvad tænker Emil, 11 år?",
+      hint: "Hint: Find svaret i Sofies Verden s. 95, tredje afsnit",
+    },
   },
 
   {
-    question:
-      "Hvordan reagerer fangerne?",
+    question: "Hvordan reagerer fangerne?",
     answers: [
-      {
-        text: "De bliver nysgerrige og følger med",
-        correct: false,
-      },
-      {
-        text: "De forlader straks hulen",
-        correct: false,
-      },
-      {
-        text: "De tror det er løgn og afviser det",
-        correct: true,
-      },
+      { text: "De forlader straks hulen", correct: false },
+      { text: "Det tager lang tid at overbevise dem", correct: false },
+      { text: "De tror ikke den frigjorte og dræber ham", correct: true },
     ],
+    hints: {
+      audioText: "Hvad tænker Ida, 10 år?",
+      hint: "Hint: Find svaret i Sofies Verden s. 95, fjerde afsnit",
+    },
   },
 ];
 
